8e97515f9207e4c1872463a91dd35e7582e59ef1
Jussi <Jussi@Echelon>
2016-07-09 00:57:16 +0300

    Pom updated to include Hortonworks dependencies

---
+++ src/main/java/com/accenture/hadoop/mapredtrial/MapredTrial.java
@@ -0,0 +1,13 @@
+package com.accenture.hadoop.mapredtrial;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class MapredTrial {
+	
+	public class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
+		
+	}
+
+}
